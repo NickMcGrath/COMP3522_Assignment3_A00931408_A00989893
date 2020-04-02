18737e0c5bae7f03aa3f1fd3b17e8345a80e72b3
eHAce <nickmcgrath@live.ca>
2020-04-02 00:20:29 -0700

    added command line argument part

---
+++ start_mod_remove_later.py
@@ -0,0 +1,13 @@
+class Request:
+    """
+    contains all the data gathered by the argparse module
+    """
+    pass
+
+
+class PokedexObject:
+    """
+    base class that defines the name and id parameter. The pokemon, moves,
+    stat, and ability classes should inherit from this class.
+    """
+    pass
