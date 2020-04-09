95ea3a69ba425f4ff0a7952a5fcb2d00420e0f9d
sarah-ryoo <47673592+sarah-ryoo@users.noreply.github.com>
2020-04-08 21:28:16 -0700

    Added init and str methods to classes

--- pokeretriever/pokeretriever.py
+++ pokeretriever/pokeretriever.py
@@ -31,20 +31,79 @@ class Request:
 
 
 class PokedexObject:
-    pass
+    def __init__(self, name: str, id_: int):
+        """Initialize moves."""
+        self.name = name
+        self.id = id_
+
+    def __str__(self):
+        """Returns the current state of the Move"""
+        return f'PokedexObject={str(vars(self))}'
 
 
 class Pokemon(PokedexObject):
-    pass
+    def __init__(self, name: str, id_: int, height: int, weight: int, stats,
+                 types: list, abilities, move):
+        """Initialize moves."""
+        self.name = name
+        self.id = id_
+        self.height = height
+        self.weight = weight
+        self.stats = stats
+        self.types = types
+        self.abilities = abilities
+        self.move = move
+
+    def __str__(self):
+        """Returns the current state of the Move"""
+        return f'Pokemon={str(vars(self))}'
 
 
 class Ability(PokedexObject):
-    pass
+    def __init__(self, name: str, id_: int, generation: str, effect: str,
+                 effect_short: str, Pokemon: list):
+        """Initialize moves."""
+        self.name = name
+        self.id = id_
+        self.generation = generation
+        self.effect = effect
+        self.effect_short = effect_short
+        self.Pokemon = Pokemon
+
+    def __str__(self):
+        """Returns the current state of the Move"""
+        return f'current state of Ability={str(vars(self))}'
 
 
 class Move(PokedexObject):
-    pass
+    """Moves represents the moves of a specific pokemon."""
+
+    def __init__(self, name: str, id_: int, generation: str, accuracy: int,
+                 pp: int, power: int, type_: str, damage_class: str,
+                 effect_short: str):
+        """Initialize moves."""
+        self.name = name
+        self.id = id_
+        self.generation = generation
+        self.accuracy = accuracy
+        self.pp = pp
+        self.power = power
+        self.type = type_
+        self.damage_class = damage_class
+        self.effect_short = effect_short
+
+    def __str__(self):
+        """Returns the current state of the Move"""
+        return f'current state of Moves={str(vars(self))}'
 
 
 class Stat(PokedexObject):
-    pass
+    def __init__(self, name: str, id_: int, is_battle_only: bool):
+        """Initialize moves."""
+        self.name = name
+        self.id = id_
+        self.is_battle_only = is_battle_only
+
+    def __str__(self):
+        """Returns the current state of the Move"""
+        return f'current state of Stat={str(vars(self))}'
