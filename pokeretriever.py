30c59c00628ef7513703bf27966c4d8297039bd9
sarah-ryoo <47673592+sarah-ryoo@users.noreply.github.com>
2020-04-08 17:02:43 -0700

    Initial commit

---
+++ pokeretriever.py
@@ -0,0 +1,23 @@
+"""
+This module contains the code required to create an aiohttp session and
+execute requests, parse the JSON and instantiate the appropriate object,
+and houses the Pokemon, Ability, Move, and Stat classes.
+"""
+import aiohttp
+
+
+class Pokemon:
+    pass
+
+
+class Ability:
+    pass
+
+
+class Move:
+    pass
+
+
+class Stat:
+    pass
+
